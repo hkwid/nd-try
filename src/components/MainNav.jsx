d3ac1a70105d118db83bc02836e06a4bdaf8a2d2
hkwid <hrnr.kwid@gmail.com>
2016-08-17 17:29:44 +0200

    Add MainNav component

---
+++ src/components/MainNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const MainNav = () => (
+  <AppBar
+    title="Dashboard"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+);
+
+export default MainNav;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MainNav from './MainNav.jsx';
 
-module.exports = React.createClass({
-  getInitialState() {
-    return {
-      text: 'Hello world!'
+injectTapEventPlugin();
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'hello world'
     };
-  },
+  }
 
   render() {
     return (
-      <h1>{this.state.text}</h1>
-    )
+      <MuiThemeProvider>
+        <div>
+          <MainNav />
+          <h1>{this.state.text}</h1>
+        </div>
+      </MuiThemeProvider>
+    );
   }
-});
+}
+
+export default Main;
